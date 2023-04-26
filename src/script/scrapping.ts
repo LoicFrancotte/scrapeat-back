ed8b6b99eee3ef30d9055ecbb46071983e35fa5d
LoicFrancotte <loic880@hotmail.com>
2023-04-26 10:50:14 +0200

    debug script for deploy

--- src/script/scrapping.ts
+++ src/script/scrapping.ts
@@ -1,10 +1,7 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: false,
-  });
+  const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
 
